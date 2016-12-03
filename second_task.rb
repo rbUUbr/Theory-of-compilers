65ff29199ef32644376a94aaca25399377585ba7
rbuDeveloper <kirabsuir@gmail.com>
2016-12-03 21:07:12 +0300

    Cont.

--- second_task.rb
+++ second_task.rb
@@ -3,12 +3,17 @@ require './binary_tree'
 include BinaryTree
 
 $root = Node.new("<sentence>")
+puts "Do you hear me?"
 %w{Do you hear me?}.each.with_index do |word, index|
     $root.insert(Node.new(word), index)
 end
-puts "Vп= {<sentence>, <subject>, <predicate>, <pronoun>}"
+Vps= [, "<noun>", "<subject>", "<predicate>", "<pronoun>", "<sentence>"]
 $root.each do |node|
   puts "#{node.word} (#{node.count})"
+end
+i = 1
+$root.each do |node|
+	i += 1 
 end
 puts "#{$root.size} words. "
 puts "#{$root.count_all} nodes."
